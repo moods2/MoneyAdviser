f31cb659464658c39ce22e5a488879aaead00c0b
DaeWoong Yoon <aormsjahs@gmail.com>
2020-07-06 18:24:57 +0900

    Create 부산방면 KTX.sql

---
+++ [04] Script/02.DML Script/교통수단/부산방면 KTX.sql
@@ -0,0 +1,62 @@
+insert into tbltrain values (21, 'KTX', to_date('05:15', 'hh24:mi'), '49800', '서울', '부산', 1, 1, to_date('07:51', 'hh24:mi'), 21);
+insert into tbltrain values (22, 'KTX', to_date('05:15', 'hh24:mi'), '49800', '서울', '부산', 1, 1, to_date('07:51', 'hh24:mi'), 22);
+insert into tbltrain values (23, 'KTX', to_date('06:00', 'hh24:mi'), '49800', '서울', '부산', 1, 1, to_date('08:37', 'hh24:mi'), 23);
+insert into tbltrain values (24, 'KTX', to_date('07:00', 'hh24:mi'), '49800', '서울', '부산', 1, 1, to_date('09:40', 'hh24:mi'), 24);
+insert into tbltrain values (25, 'KTX', to_date('08:00', 'hh24:mi'), '49800', '서울', '부산', 1, 1, to_date('10:43', 'hh24:mi'), 25);
+insert into tbltrain values (26, 'KTX', to_date('09:45', 'hh24:mi'), '49800', '서울', '부산', 1, 1, to_date('12:49', 'hh24:mi'), 26);
+insert into tbltrain values (27, 'KTX', to_date('10:17', 'hh24:mi'), '49800', '서울', '부산', 1, 1, to_date('13:34', 'hh24:mi'), 27);
+insert into tbltrain values (28, 'KTX', to_date('12:00', 'hh24:mi'), '49800', '서울', '부산', 1, 1, to_date('14:42', 'hh24:mi'), 28);
+insert into tbltrain values (29, 'KTX', to_date('13:05', 'hh24:mi'), '49800', '서울', '부산', 1, 1, to_date('16:13', 'hh24:mi'), 29);
+insert into tbltrain values (30, 'KTX', to_date('14:00', 'hh24:mi'), '49800', '서울', '부산', 1, 1, to_date('16:27', 'hh24:mi'), 30);
+
+
+insert into tbltrain values (, 'KTX', to_date('06:12', 'hh24:mi'), '49800', '광명', '부산', 1, 1, to_date('07:51', 'hh24:mi'), 21);
+insert into tbltrain values (, 'KTX', to_date('06:58', 'hh24:mi'), '49800', '대전', '부산', 1, 1, to_date('07:51', 'hh24:mi'), 21);
+insert into tbltrain values (, 'KTX', to_date('07:51', 'hh24:mi'), '49800', '대구', '부산', 1, 1, to_date('07:51', 'hh24:mi'), 21);
+
+insert into tbltrain values (, 'KTX', to_date('06:42', 'hh24:mi'), '49800', '광명', '부산', 1, 1, to_date('08:21', 'hh24:mi'), 22);
+insert into tbltrain values (, 'KTX', to_date('07:28', 'hh24:mi'), '49800', '대전', '부산', 1, 1, to_date('08:21', 'hh24:mi'), 22);
+insert into tbltrain values (, 'KTX', to_date('08:21', 'hh24:mi'), '49800', '대구', '부산', 1, 1, to_date('08:21', 'hh24:mi'), 22);
+
+
+
+
+insert into tbltrain values (, 'KTX', to_date('06:16', 'hh24:mi'), '49800', '광명', '부산', 1, 1, to_date('08:37', 'hh24:mi'), 23);
+insert into tbltrain values (, 'KTX', to_date('06:38', 'hh24:mi'), '49800', '천안', '부산', 1, 1, to_date('08:37', 'hh24:mi'), 23);
+insert into tbltrain values (, 'KTX', to_date('07:13', 'hh24:mi'), '49800', '대전', '부산', 1, 1, to_date('08:37', 'hh24:mi'), 23);
+insert into tbltrain values (, 'KTX', to_date('08:05', 'hh24:mi'), '49800', '대구', '부산', 1, 1, to_date('08:37', 'hh24:mi'), 23);
+
+insert into tbltrain values (, 'KTX', to_date('07:16', 'hh24:mi'), '49800', '광명', '부산', 1, 1, to_date('09:40', 'hh24:mi'), 24);
+insert into tbltrain values (, 'KTX', to_date('07:38', 'hh24:mi'), '49800', '천안', '부산', 1, 1, to_date('09:40', 'hh24:mi'), 24);
+insert into tbltrain values (, 'KTX', to_date('08:08', 'hh24:mi'), '49800', '대전', '부산', 1, 1, to_date('09:40', 'hh24:mi'), 24);
+insert into tbltrain values (, 'KTX', to_date('08:54', 'hh24:mi'), '49800', '대구', '부산', 1, 1, to_date('09:40', 'hh24:mi'), 24);
+
+insert into tbltrain values (, 'KTX', to_date('08:17', 'hh24:mi'), '49800', '광명', '부산', 1, 1, to_date('10:43', 'hh24:mi'), 25);
+insert into tbltrain values (, 'KTX', to_date('09:00', 'hh24:mi'), '49800', '대전', '부산', 1, 1, to_date('10:43', 'hh24:mi'), 25);
+insert into tbltrain values (, 'KTX', to_date('09:50', 'hh24:mi'), '49800', '대구', '부산', 1, 1, to_date('10:43', 'hh24:mi'), 25);
+
+
+insert into tbltrain values (, 'KTX', to_date('10:02', 'hh24:mi'), '49800', '광명', '부산', 1, 1, to_date('12:49', 'hh24:mi'), 26);
+insert into tbltrain values (, 'KTX', to_date('10:24', 'hh24:mi'), '49800', '천안', '부산', 1, 1, to_date('12:49', 'hh24:mi'), 26);
+insert into tbltrain values (, 'KTX', to_date('10:50', 'hh24:mi'), '49800', '대전', '부산', 1, 1, to_date('12:49', 'hh24:mi'), 26);
+insert into tbltrain values (, 'KTX', to_date('11:37', 'hh24:mi'), '49800', '대구', '부산', 1, 1, to_date('12:49', 'hh24:mi'), 26);
+
+insert into tbltrain values (, 'KTX', to_date('11:59', 'hh24:mi'), '49800', '대전', '부산', 1, 1, to_date('13:34', 'hh24:mi'), 27);
+insert into tbltrain values (, 'KTX', to_date('12:45', 'hh24:mi'), '49800', '대구', '부산', 1, 1, to_date('13:34', 'hh24:mi'), 27);
+
+insert into tbltrain values (, 'KTX', to_date('12:16', 'hh24:mi'), '49800', '광명', '부산', 1, 1, to_date('14:42', 'hh24:mi'), 28);
+insert into tbltrain values (, 'KTX', to_date('12:39', 'hh24:mi'), '49800', '천안', '부산', 1, 1, to_date('14:42', 'hh24:mi'), 28);
+insert into tbltrain values (, 'KTX', to_date('13:03', 'hh24:mi'), '49800', '대전', '부산', 1, 1, to_date('14:42', 'hh24:mi'), 28);
+insert into tbltrain values (, 'KTX', to_date('13:49', 'hh24:mi'), '49800', '대구', '부산', 1, 1, to_date('14:42', 'hh24:mi'), 28);
+
+insert into tbltrain values (, 'KTX', to_date('13:22', 'hh24:mi'), '49800', '광명', '부산', 1, 1, to_date('16:13', 'hh24:mi'), 29);
+insert into tbltrain values (, 'KTX', to_date('13:45', 'hh24:mi'), '49800', '천안', '부산', 1, 1, to_date('16:13', 'hh24:mi'), 29);
+insert into tbltrain values (, 'KTX', to_date('14:09', 'hh24:mi'), '49800', '대전', '부산', 1, 1, to_date('16:13', 'hh24:mi'), 29);
+insert into tbltrain values (, 'KTX', to_date('15:01', 'hh24:mi'), '49800', '대구', '부산', 1, 1, to_date('16:13', 'hh24:mi'), 29);
+
+insert into tbltrain values (, 'KTX', to_date('14:16', 'hh24:mi'), '49800', '광명', '부산', 1, 1, to_date('16:27', 'hh24:mi'), 30);
+insert into tbltrain values (, 'KTX', to_date('15:02', 'hh24:mi'), '49800', '대전', '부산', 1, 1, to_date('16:27', 'hh24:mi'), 30);
+insert into tbltrain values (, 'KTX', to_date('15:47', 'hh24:mi'), '49800', '대구', '부산', 1, 1, to_date('16:27', 'hh24:mi'), 30);
+
+
+
