f305bdcb4efee096e4c23bc1aa6889ea6babfd3f
DaeWoong Yoon <aormsjahs@gmail.com>
2020-07-06 19:27:22 +0900

    Create 새마을 서울방면.sql

---
+++ [04] Script/02.DML Script/교통수단/새마을 서울방면.sql
@@ -0,0 +1,85 @@
+insert into tbltrain values (48, 'ITX-새마을', to_date('06:16', 'hh24:mi'), '49800', '부산', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+insert into tbltrain values (49, 'ITX-새마을', to_date('09:16', 'hh24:mi'), '49800', '부산', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+insert into tbltrain values (50, 'ITX-새마을', to_date('11:05', 'hh24:mi'), '49800', '부산', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+insert into tbltrain values (51, 'ITX-새마을', to_date('11:45', 'hh24:mi'), '49800', '부산', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+insert into tbltrain values (52, 'ITX-새마을', to_date('14:03', 'hh24:mi'), '49800', '부산', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+insert into tbltrain values (53, 'ITX-새마을', to_date('15:55', 'hh24:mi'), '49800', '부산', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+insert into tbltrain values (54, 'ITX-새마을', to_date('20:25', 'hh24:mi'), '49800', '부산', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
+
+
+insert into tbltrain values (, 'ITX-새마을', to_date('06:54', 'hh24:mi'), '49800', '밀양', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+insert into tbltrain values (, 'ITX-새마을', to_date('07:30', 'hh24:mi'), '49800', '동대구', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+insert into tbltrain values (, 'ITX-새마을', to_date('07:36', 'hh24:mi'), '49800', '대구', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+insert into tbltrain values (, 'ITX-새마을', to_date('08:05', 'hh24:mi'), '49800', '구미', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+insert into tbltrain values (, 'ITX-새마을', to_date('08:20', 'hh24:mi'), '49800', '김천', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+insert into tbltrain values (, 'ITX-새마을', to_date('09:13', 'hh24:mi'), '49800', '대전', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+insert into tbltrain values (, 'ITX-새마을', to_date('09:37', 'hh24:mi'), '49800', '조치원', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+insert into tbltrain values (, 'ITX-새마을', to_date('09:59', 'hh24:mi'), '49800', '천안', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+insert into tbltrain values (, 'ITX-새마을', to_date('10:28', 'hh24:mi'), '49800', '수원', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+insert into tbltrain values (, 'ITX-새마을', to_date('10:49', 'hh24:mi'), '49800', '영등포', '서울', 1, 1, to_date('10:58', 'hh24:mi'), 48);
+
+insert into tbltrain values (, 'ITX-새마을', to_date('09:54', 'hh24:mi'), '49800', '밀양', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+insert into tbltrain values (, 'ITX-새마을', to_date('10:30', 'hh24:mi'), '49800', '동대구', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+insert into tbltrain values (, 'ITX-새마을', to_date('10:36', 'hh24:mi'), '49800', '대구', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+insert into tbltrain values (, 'ITX-새마을', to_date('11:05', 'hh24:mi'), '49800', '구미', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+insert into tbltrain values (, 'ITX-새마을', to_date('11:20', 'hh24:mi'), '49800', '김천', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+insert into tbltrain values (, 'ITX-새마을', to_date('12:13', 'hh24:mi'), '49800', '대전', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+insert into tbltrain values (, 'ITX-새마을', to_date('12:37', 'hh24:mi'), '49800', '조치원', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+insert into tbltrain values (, 'ITX-새마을', to_date('12:59', 'hh24:mi'), '49800', '천안', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+insert into tbltrain values (, 'ITX-새마을', to_date('13:28', 'hh24:mi'), '49800', '수원', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+insert into tbltrain values (, 'ITX-새마을', to_date('13:49', 'hh24:mi'), '49800', '영등포', '서울', 1, 1, to_date('13:56', 'hh24:mi'), 49);
+
+insert into tbltrain values (, 'ITX-새마을', to_date('11:45', 'hh24:mi'), '49800', '밀양', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+insert into tbltrain values (, 'ITX-새마을', to_date('12:21', 'hh24:mi'), '49800', '동대구', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+insert into tbltrain values (, 'ITX-새마을', to_date('12:27', 'hh24:mi'), '49800', '대구', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+insert into tbltrain values (, 'ITX-새마을', to_date('12:56', 'hh24:mi'), '49800', '구미', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+insert into tbltrain values (, 'ITX-새마을', to_date('13:11', 'hh24:mi'), '49800', '김천', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+insert into tbltrain values (, 'ITX-새마을', to_date('14:04', 'hh24:mi'), '49800', '대전', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+insert into tbltrain values (, 'ITX-새마을', to_date('14:28', 'hh24:mi'), '49800', '조치원', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+insert into tbltrain values (, 'ITX-새마을', to_date('14:50', 'hh24:mi'), '49800', '천안', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+insert into tbltrain values (, 'ITX-새마을', to_date('15:19', 'hh24:mi'), '49800', '수원', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+insert into tbltrain values (, 'ITX-새마을', to_date('15:40', 'hh24:mi'), '49800', '영등포', '서울', 1, 1, to_date('15:47', 'hh24:mi'), 50);
+
+insert into tbltrain values (, 'ITX-새마을', to_date('12:31', 'hh24:mi'), '49800', '밀양', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+insert into tbltrain values (, 'ITX-새마을', to_date('13:07', 'hh24:mi'), '49800', '동대구', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+insert into tbltrain values (, 'ITX-새마을', to_date('13:13', 'hh24:mi'), '49800', '대구', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+insert into tbltrain values (, 'ITX-새마을', to_date('13:42', 'hh24:mi'), '49800', '구미', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+insert into tbltrain values (, 'ITX-새마을', to_date('13:57', 'hh24:mi'), '49800', '김천', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+insert into tbltrain values (, 'ITX-새마을', to_date('14:50', 'hh24:mi'), '49800', '대전', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+insert into tbltrain values (, 'ITX-새마을', to_date('15:14', 'hh24:mi'), '49800', '조치원', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+insert into tbltrain values (, 'ITX-새마을', to_date('15:36', 'hh24:mi'), '49800', '천안', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+insert into tbltrain values (, 'ITX-새마을', to_date('16:04', 'hh24:mi'), '49800', '수원', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+insert into tbltrain values (, 'ITX-새마을', to_date('16:26', 'hh24:mi'), '49800', '영등포', '서울', 1, 1, to_date('16:33', 'hh24:mi'), 51);
+
+insert into tbltrain values (, 'ITX-새마을', to_date('14:44', 'hh24:mi'), '49800', '밀양', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+insert into tbltrain values (, 'ITX-새마을', to_date('15:20', 'hh24:mi'), '49800', '동대구', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+insert into tbltrain values (, 'ITX-새마을', to_date('15:26', 'hh24:mi'), '49800', '대구', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+insert into tbltrain values (, 'ITX-새마을', to_date('15:55', 'hh24:mi'), '49800', '구미', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+insert into tbltrain values (, 'ITX-새마을', to_date('16:10', 'hh24:mi'), '49800', '김천', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+insert into tbltrain values (, 'ITX-새마을', to_date('17:13', 'hh24:mi'), '49800', '대전', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+insert into tbltrain values (, 'ITX-새마을', to_date('17:27', 'hh24:mi'), '49800', '조치원', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+insert into tbltrain values (, 'ITX-새마을', to_date('17:49', 'hh24:mi'), '49800', '천안', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+insert into tbltrain values (, 'ITX-새마을', to_date('18:17', 'hh24:mi'), '49800', '수원', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+insert into tbltrain values (, 'ITX-새마을', to_date('18:39', 'hh24:mi'), '49800', '영등포', '서울', 1, 1, to_date('18:46', 'hh24:mi'), 52);
+
+insert into tbltrain values (, 'ITX-새마을', to_date('16:24', 'hh24:mi'), '49800', '밀양', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+insert into tbltrain values (, 'ITX-새마을', to_date('17:00', 'hh24:mi'), '49800', '동대구', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+insert into tbltrain values (, 'ITX-새마을', to_date('17:06', 'hh24:mi'), '49800', '대구', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+insert into tbltrain values (, 'ITX-새마을', to_date('17:35', 'hh24:mi'), '49800', '구미', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+insert into tbltrain values (, 'ITX-새마을', to_date('17:50', 'hh24:mi'), '49800', '김천', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+insert into tbltrain values (, 'ITX-새마을', to_date('18:53', 'hh24:mi'), '49800', '대전', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+insert into tbltrain values (, 'ITX-새마을', to_date('19:07', 'hh24:mi'), '49800', '조치원', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+insert into tbltrain values (, 'ITX-새마을', to_date('19:29', 'hh24:mi'), '49800', '천안', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+insert into tbltrain values (, 'ITX-새마을', to_date('19:57', 'hh24:mi'), '49800', '수원', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+insert into tbltrain values (, 'ITX-새마을', to_date('20:19', 'hh24:mi'), '49800', '영등포', '서울', 1, 1, to_date('20:26', 'hh24:mi'), 53);
+
+insert into tbltrain values (, 'ITX-새마을', to_date('20:57', 'hh24:mi'), '49800', '밀양', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
+insert into tbltrain values (, 'ITX-새마을', to_date('21:33', 'hh24:mi'), '49800', '동대구', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
+insert into tbltrain values (, 'ITX-새마을', to_date('21:39', 'hh24:mi'), '49800', '대구', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
+insert into tbltrain values (, 'ITX-새마을', to_date('22:08', 'hh24:mi'), '49800', '구미', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
+insert into tbltrain values (, 'ITX-새마을', to_date('22:23', 'hh24:mi'), '49800', '김천', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
+insert into tbltrain values (, 'ITX-새마을', to_date('23:26', 'hh24:mi'), '49800', '대전', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
+insert into tbltrain values (, 'ITX-새마을', to_date('23:40', 'hh24:mi'), '49800', '조치원', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
+insert into tbltrain values (, 'ITX-새마을', to_date('00:02', 'hh24:mi'), '49800', '천안', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
+insert into tbltrain values (, 'ITX-새마을', to_date('00:30', 'hh24:mi'), '49800', '수원', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
+insert into tbltrain values (, 'ITX-새마을', to_date('00:52', 'hh24:mi'), '49800', '영등포', '서울', 1, 1, to_date('00:59', 'hh24:mi'), 54);
