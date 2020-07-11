ddbefa7003f3154587c672e930ccd12451d538cd
moods2 <65211288+moods2@users.noreply.github.com>
2020-07-11 18:01:16 +0900

    Update BusTrain.java

--- [05] 프로젝트소스/Yoginolja/src/main/BusTrain.java
+++ [05] 프로젝트소스/Yoginolja/src/main/BusTrain.java
@@ -44,7 +44,6 @@ public class BusTrain {
 					
 					rs = stat.executeQuery(sql);
 					System.out.println("================================================================================================================================");
-//					System.out.println("[과정번호]\t\t\t\t[과정명]\t\t\t\t[과정기간]\t\t[교사명]\t[강의실]");
 					while (rs.next()) { //역번호, 역이름  출발기준
 						
 						String result = String.format("%s\t%s",rs.getString("역번호"),rs.getString("역이름"));				
@@ -183,55 +182,101 @@ public class BusTrain {
 					e.printStackTrace();
 				}
 				} else if (sel.equals("2")) {
-					
-					System.out.println("---------------------------------------------------------");
-					System.out.println("1 : 		[1 강의실]");
-					System.out.println("2 : 		[2 강의실]");
-					System.out.println("3 : 		[3 강의실]");
-					System.out.println("4 : 		[4 강의실]");
-					System.out.println("5 : 		[5 강의실]");
-					System.out.println("6 : 		[6 강의실]");
-					System.out.println("---------------------------------------------------------");
-					System.out.print("기자재조회강의실 : ");
-					String num2 = scan.nextLine();
-					
 					try {
 						conn = util.open();
 						stat = conn.createStatement();
-						rs = null;
 						
-						String sql = String.format("SELECT ti.item_seq as 기자재번호, ti.name as 기자재명, ti.itemqty as 기자재수량, ti.room_seq as 강의실" + 
-								"    FROM tblItem ti" + 
-								"        INNER JOIN tblroom tr" + 
-								"            ON tr.room_seq = ti.room_seq" + 
-								"                INNER JOIN tblCourselist cl" + 
-								"                    ON cl.Courselist_seq = ti.Courselist_seq" + 
-								"                        WHERE ti.room_seq = %s ", num2);
-												
+						String sql = "select busstation_seq as 터미널번호, busstationloca as 지역, busstationname as 터미널명 from tblbusstation;";
+						
 						rs = stat.executeQuery(sql);
-						System.out.println("============================================================");
-						System.out.println("[기자재번호]\t[기자재명]\t[기자재수량]\t[강의실]");
-						while (rs.next()) { //기자재번호, 기자재명, 기자재수량, 강의실
+						System.out.println("================================================================================================================================");
+						while (rs.next()) { //역번호, 지역, 터미널이름
 							
-//							String result = String.format("%5s\t\t%6s\t%6s\t\t%5s", rs.getString("기자재번호"),  rs.getString("기자재명"), rs.getString("기자재수량")
-//							,rs.getString("강의실"));
-							String result = String.format("%-5s\t\t%-6s\t%-6s\t\t%-5s", rs.getString("기자재번호"),  rs.getString("기자재명"), rs.getString("기자재수량")
-							,rs.getString("강의실"));
-					
+							String result = String.format("%s\t%s",rs.getString("터미널번호"),rs.getString("지역"),rs.getString("터미널명"));				
+
+
+							
 							System.out.println(result);
-								
 						}
-						System.out.println("============================================================");
-						rs.close();							
+						System.out.println("================================================================================================================================");
+						
+						System.out.print("출발 버스터미널 : ");
+						String startbus = scan.nextLine();
+						
+						System.out.println();
+						System.out.println();
 						conn.close();
 						stat.close();
 						
-						
-						loop = false;
-						
-						System.out.println();
-						System.out.print("계속하시려면 엔터를 눌러주세요");						
-						String num3 = scan.nextLine();
+						try {
+							conn = util.open();
+							stat = conn.createStatement();
+							
+							String sql2 = "select busstation_seq as 터미널번호, busstationloca as 지역, busstationname as 터미널명 from tblbusstation;";
+							
+							rs = stat.executeQuery(sql2);
+							System.out.println("================================================================================================================================");
+							while (rs.next()) { //역번호, 지역, 터미널이름
+								
+								String result = String.format("%s\t%s",rs.getString("터미널번호"),rs.getString("지역"),rs.getString("터미널명"));				
+
+
+								
+								System.out.println(result);
+							}
+							System.out.println("========================================================================================================================");
+							System.out.print("도착 버스터미널번호 : ");
+							String arrivebus = scan.nextLine();							
+							System.out.println();
+							System.out.println();
+//							rs.close();							
+							conn.close();
+							stat.close();		
+							
+							try {
+								conn = util.open();
+								stat = conn.createStatement();
+								rs = null;
+								
+								String sql3 = String.format("select DISTINCT bus_seq " + 
+										"    as 버스번호 ,d.busstationloca as 출발지, to_char(busstarttime,'hh24:mi') as 출발시간,bustime as 소요시간,  e.*,  busprice as 가격 " + 
+										"        from (select DISTINCT b.busstationloca as 도착지     "+ 
+										"        from tblbus a" + 
+										"        inner join tblbusstation b" + 
+										"            on a.abusstation_seq2 = b.busstation_seq" + 
+										"            where a.sbusstation_seq = %s and a.abusstation_seq2 = %s) e," + 
+										"        tblbus c "+ 
+										"        inner join tblbusstation d" + 
+										"            on c.sbusstation_seq = d.busstation_seq" + 
+										"             where c.sbusstation_seq = %s and c.abusstation_seq2 = %s" + 
+										"                order by c.bus_seq;", startbus,arrivebus,startbus,arrivebus );
+
+
+														
+								rs = stat.executeQuery(sql3);
+								System.out.println("================================================================================================================================");
+								System.out.println("[버스번호]\t[출발지]\t[출발시간]\t[소요시간]\t[도착지]\t[가격]");
+								while (rs.next()) { //역번호, 역이름  도착기준
+									
+									String result = String.format("%s\t%s\t%s\t%s\t%s\t%s",rs.getString("버스번호"),rs.getString("출발지"),rs.getString("출발시간"),rs.getString("소요시간"),rs.getString("도착지"),rs.getString("가격"));				
+
+									
+									
+									System.out.println(result);		
+								}
+								System.out.println("========================================================================================================================");
+								System.out.print("예매하기 : ");
+								String busreserv = scan.nextLine();
+								rs.close();							
+								conn.close();
+								stat.close();
+							} catch (Exception e) {
+								// TODO: handle exception
+							}
+							
+						} catch (Exception e) {
+							// TODO: handle exception
+						}
 					} catch (Exception e) {
 						e.printStackTrace();
 						// TODO: handle exception
